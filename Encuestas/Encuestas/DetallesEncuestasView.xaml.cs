12c7bbec0d1b1f9a87922f9d0f7249e9aa001fea
Tatiana-Ruiz-Rodriguez <tatianaruiz@compucentroltda.com>
2018-05-15 23:08:05 -0500

    Encuestas

---
+++ Encuestas/Encuestas/DetallesEncuestasView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Encuestas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetallesEncuestasView : ContentPage
+	{
+        private readonly string[] equipos = { "Colombia", "peru", "brasil"};
+
+		public DetallesEncuestasView ()
+		{
+			InitializeComponent ();
+            BtAgregarEq.Clicked += BtAgregarEq_Clicked;
+		}
+
+        private async void BtAgregarEq_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(EntNombre.Text) || string.IsNullOrEmpty(LbEquipoFav.Text)) {
+
+                await DisplayAlert("Datos Incompletos",
+                    "Por favor ingrese los campos", "Aceptar");
+
+                return;
+            }
+
+            var enc = new Encuesta()
+        }
+    }
+}
